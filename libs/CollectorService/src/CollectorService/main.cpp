7aadfd0f479d2598bb475598030cee00c5d4049d
FBaumeister <fabian_baumeister@gmx.de>
2022-06-26 18:40:24 +0200

    Draft on CollectorService.

--- libs/CollectorService/src/CollectorService/main.cpp
+++ libs/CollectorService/src/CollectorService/main.cpp
@@ -5,5 +5,19 @@ int main(int argc, char **argv)
 {
     std::cout << "DataCollector Service: WIP" << std::endl;
 
+    // Utilities - filter argv
+    // Utilities - setup console signal handler (interrupt loop and safe cleanup)
+
+    // Create DataCollector Instance
+    // Register Collectors
+
+    // Create DataPackager Instance
+    // Setup to package data on new data collection
+
+    // Create FileSystemMonitor
+    // Link: onFileCreated - trigger to DataCollector
+
+    // loop and wait for signal handler
+
     return 0;
 }
